b9a3ad34683f752dd88946882481653eec4191b1
Artem Lashmanov <lashmanov-aa@roxiemobile.com>
2023-03-19 21:22:10 +0300

    Edited 'infoPlist' values in 'Project.swift'.

--- PGPMessageApp/Project.swift
+++ PGPMessageApp/Project.swift
@@ -2,6 +2,14 @@ import ProjectDescription
 
 // MARK: - Project
 
+let infoPlist: [String: InfoPlist.Value] = [
+    "UILaunchStoryboardName": .string("LaunchScreen.storyboard"),
+    "UIApplicationSceneManifest": .dictionary([
+        "UIApplicationSupportsMultipleScenes": .boolean(false),
+        "UISceneConfigurations": .dictionary([:])
+    ])
+]
+
 let project = Project(
     name: "PGPMessageApp",
     targets: [
@@ -11,7 +19,7 @@ let project = Project(
             product: .app,
             bundleId: "ru.lashmanov.PGPMessage",
             deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
-            infoPlist: .default,
+            infoPlist: .extendingDefault(with: infoPlist),
             sources: [
                 "Sources/**"
             ],
